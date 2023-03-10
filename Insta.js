9b2762597e39528e7ac4f1f8e24af147eff5ae82
safa2090 <safadridi1987@gmail.com>
2023-03-10 18:38:14 +0100

    change

--- Insta.js
+++ Insta.js
@@ -1,29 +1,4 @@
-/*var fashionArray = [];
-var travelArray = [];
-var cookingArray = [];
-var sarcasmArray = [];
-
-
-$(document).ready(function() {
-
-  $(".fashion img").each(function() {
-    fashionArray.push($(this).attr("src"));
-  });
-
-  $(".travel img").each(function() {
-    travelArray.push($(this).attr("src"));
-  });
-
-  $(".cooking img").each(function() {
-    cookingArray.push($(this).attr("src"));
-  });
-
-  $(".sarcasm img").each(function() {
-    sarcasmArray.push($(this).attr("src"));
-  });})*/
-
   $(document).ready(function() {
-
     $('.but').click(function(){
       $(this).addClass('buttons').siblings().removeClass('buttons');
       var filter=$(this).attr('data-filter')
